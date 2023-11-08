01b31328f9846ec1ef5484de150c88912750b1e2
Leonid-Ro <leonidrozdestvencky@gmail.com>
2023-11-08 19:48:59 +0300

    8.11.23

--- Операторы присваивания/app.js
+++ Операторы присваивания/app.js
@@ -1,4 +1,19 @@
 // Операторы присваивания
 let age = 18+5;
 age += 2; // age = age + 2
+age -= 3; // age = age - 3
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
+
+age ++; // age = age + 1
+age --; // age = age -1
+
 console.log(age);
+
+// Операторы сравнения
+const vasya = 20;
+console.log(age > vasya);
+console.log(age >= vasya);
+console.log(age < vasya);
+console.log(age <= vasya);
+console.log(age == vasya);
