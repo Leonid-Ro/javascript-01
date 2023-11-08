78b4898e8107ec99d1cc0e0974acf694dbd60f86
Leonid-Ro <leonidrozdestvencky@gmail.com>
2023-11-08 18:51:21 +0300

    Операторы присваивания

---
+++ Базовые операторы/app.js
@@ -0,0 +1,17 @@
+//Базовые операторы
+
+/*
+Две
+строки
+и далее
+*/
+const width = 10;
+const heyght = 5;
+const space = width*heyght;
+const newWidth = width - 4;
+console.log(newWidth);
+
+//Строки
+const city = 'Moscow';
+const street = 'Фили';
+console.log(city + ', ' + street + ' ' + 5);
