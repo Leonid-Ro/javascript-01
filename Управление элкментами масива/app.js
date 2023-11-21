960b7b3c89dc0274af14fe59b65d9dc27eca014c
Leonid-Ro <leonidrozdestvencky@gmail.com>
2023-11-21 19:30:11 +0300

    21.11.2023

---
+++ Управление элкментами масива/app.js
@@ -0,0 +1,22 @@
+const users = ['Аня', 'Вика', 'Катя'];
+console.log(users);
+users[2] = 'Кристина';// Замена
+console.log(users);
+users[3] = 'Никита';// Добавление
+console.log(users);
+
+const friend = users.push('Саша');// Добавление в конец
+console.log(friend);// Возвращает длинну обновленного массива
+console.log(users);
+
+const friend1 = users.unshift('Денис');// Добавление в начало
+console.log(friend1);// Возвращает длинну обновленного массива
+console.log(users);
+
+const friend2 = users.pop();// Удаляет последний элемент
+console.log(friend2);// Возвращает поледний элемент
+console.log(users);
+
+const friend3 = users.shift();// Удаляет первый элемент
+console.log(friend3);// Возвращает первый элемент
+console.log(users);
