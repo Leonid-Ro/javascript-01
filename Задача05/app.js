c6949742a80012b2554f828e510194fbb40592d1
Leonid-Ro <leonidrozdestvencky@gmail.com>
2023-11-21 19:31:21 +0300

    21.11.23

--- Задача05/app.js
+++ Задача05/app.js
@@ -1,21 +1,25 @@
 function canBuy(age, job) {
-    if (age > 24 && job == true) {
+    age > 24 && job == true ? credit = 500 : age > 24 ? credit = 100 : credit = 0;
+}
+function result(money) {
+    canBuy(25, false);
+    (credit + money) >= 2000 ? res = true : res = false;
+}
+result(1900)
+console.log(res);
+/*
+if (age > 24 && job == true) {
         credit = 500;
     } else if (age > 24) {
         credit = 100;
     } else {
         credit = 0;
     }
-    return credit;
-}
-
-function result(money) {
-    canBuy(25, false);
-    if ((credit + money) >= 2000) {
+*/
+/*
+if ((credit + money) >= 2000) {
         return true;
     } else {
         return false;
     }
-}
-console.log(result(1900));
-
+*/
---
+++ Знакомство с массивами/app.js
@@ -0,0 +1,17 @@
+const role1 = 'Admin';
+const role2 = 'User';
+const role3 = 'Superuser';
+
+const roles = ['Admin', 'User', 'Superuser'];
+console.log(roles);
+console.log(roles[0]);
+console.log(roles.length);
+console.log(roles[roles.length - 1]);
+console.log(roles.at(0));
+console.log(roles.at(-1));
+
+const userAge = [2044 - 2023, 10 > 3 ? 'Ах!' : 'Ух!'];
+console.log(userAge);
+
+const userName = new Array('Катя', 'Петя', 'Вася');
+console.log(userName);
