764a847db5393bdea0e50d4f77eb7c76ee1865d1
Leonid-Ro <leonidrozdestvencky@gmail.com>
2023-11-23 10:15:34 +0300

    22.11.23

---
+++ Поиск элементов/app.js
@@ -0,0 +1,10 @@
+const roles = ['admin', 'user', 'manager'];
+const elIndex = roles.indexOf('admin');// Возвращает номер элемента в массиве.
+console.log(elIndex);
+const elIndex1 = roles.indexOf('superuser');
+console.log(elIndex1);
+
+roles.indexOf('admin') >= 0 ? console.log('Доступ есть!') : console.log('Отказано!');
+
+console.log(roles.includes('admin'));
+roles.includes('superuser') ? console.log('Yes!') : console.log('No!');
